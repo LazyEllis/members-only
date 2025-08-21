2eb1694f6cdec32e2f10a2778214ea7c6f8ac155
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-08-21 13:44:50 +0100

    Set up the database

--- bin/seed-db.js
+++ bin/seed-db.js
@@ -2,7 +2,31 @@
 
 import { Client } from "pg";
 
-const SQL = ``;
+const SQL = `
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  role_id INTEGER NOT NULL REFERENCES roles
+);
+
+CREATE TABLE messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO roles (name) VALUES ('USER'), ('MEMBER'), ('ADMIN');
+`;
 
 const main = async () => {
   console.log("Seeding...");
